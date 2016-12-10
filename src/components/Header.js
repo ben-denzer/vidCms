4c94e79af009534e32b7e009ec2040fdf883ae76
Ben Denzer <denzer.ben@gmail.com>
2016-12-10 03:16:06 -0800

    got a good start on the home page, images will be switched out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import {browserHistory} from 'react-router';
+import { browserHistory } from 'react-router';
 import logo from '../img/logo.png';
 
 const Header = (props) => {
     return (
-        <nav className="main-nav-container">
-            <div className="main-nav">
-                <div id="nav_logo"><img onClick={() => browserHistory.push("/")} src={logo} alt="my logo" /></div>
-                <div id="nav_buttons">{
-                    !props.name ?
-                        <div id="auth_button_group">
-                            <a type="button" id="nav_login" onClick={() => browserHistory.push("/auth/login")}>Log In</a>
-                            <a type="button" id="nav_signup" onClick={() => browserHistory.push("/auth/signup")}>Sign Up</a>
-                        </div>
-                        : <div id="auth_button_group">
-                            <a type="button" id="nav_myaccount" onClick={() => browserHistory.push("/account")}>My Account</a>
-                            <a type="button" id="nav_logout" onClick={props.logout}>Log Out</a>
-                        </div>
+        <div>
+            <nav>
+                <div className="nav-wrapper-one">
+                    <a className="brand-logo"><img onClick={() => browserHistory.push("/")} src={logo} alt="my logo" /></a>
+                    {!props.name ?
+                        <ul id="nav-mobile" className="right">
+                            <li><a type="button" id="nav_login" onClick={() => browserHistory.push("/auth/login")}>Log In</a></li>
+                            <li><a type="button" id="nav_signup" onClick={() => browserHistory.push("/auth/signup")}>Sign Up</a></li>
+                        </ul>
+                        : <ul id="nav-mobile" className="right">
+                            <li><a type="button" id="nav_myaccount" onClick={() => browserHistory.push("/account")}>My Account</a></li>
+                            <li><a type="button" id="nav_logout" onClick={props.logout}>Log Out</a></li>
+                        </ul>
                     }
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <nav>
+                <div className="nav-wrapper-two">
+                    <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+                    <ul className="center hide-on-med-and-down">
+                        <li><a href="sass.html">About</a></li>
+                        <li><a href="badges.html">Videos</a></li>
+                        <li><a href="collapsible.html">Articles</a></li>
+                        <li><a href="mobile.html">Contact</a></li>
+                    </ul>
+                    <ul className="side-nav" id="mobile-demo">
+                        <li><a href="sass.html">About</a></li>
+                        <li><a href="badges.html">Videos</a></li>
+                        <li><a href="collapsible.html">Articles</a></li>
+                        <li><a href="mobile.html">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     );
 }
 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import {browserHistory} from 'react-router';
+import slide1 from '../img/slide1.jpg';
+import slide2 from '../img/slide2.jpg';
+import slide3 from '../img/slide3.jpg';
+import slide4 from '../img/slide4.jpg';
+import team from '../img/team.jpg';
+import schedule from '../img/schedule.jpg';
+import hp_video_placeholder from '../img/hp_video_placeholder.png';
 
 const HomePage = (props) => {
     return (
         <div id="home_container">
-            <h1>Las Vegas Energy Healing</h1>
-            <h2>GET STARTED FOR FREE</h2>
-            <button
-                className="home-button"
-                id="video_button"
-                onClick={() => browserHistory.push('videos/')}
-            >
-                Go To Videos
-            </button>
+            <div className="slider">
+                <ul className="slides">
+                    <li>
+                        <img src={slide2} role="presentation" />
+                        <div className="caption left-align">
+                            <h3>e&middot;qua&middot;nim&middot;i&middot;ty</h3>
+                            <h5 className="light grey-text text-lighten-3">mental calmness, composure, and evenness of temper, especially in a difficult situation.</h5>
+                        </div>
+                    </li>
+                    <li>
+                        <img src={slide3} role="presentation" />
+                        <div className="caption right-align">
+                            <h3>e&middot;qua&middot;nim&middot;i&middot;ty</h3>
+                            <h5 className="light grey-text text-lighten-3">mental calmness, composure, and evenness of temper, especially in a difficult situation.</h5>
+                        </div>
+                    </li>
+                    <li>
+                        <img src={slide1} role="presentation" />
+                    </li>
+                    <li>
+                        <img src={slide4} role="presentation" />
+                    </li>
+                </ul>
+            </div>
+            <div id="cta_container">
+                <div className="cta-box">
+                    <img src={team} alt="Meet the Team" />
+                    <h5>Meet the Team</h5>
+                </div>
+                <div className="cta-box">
+                    <img src={hp_video_placeholder} alt="Meet the Team" />
+                    <h5>Watch</h5>
+                </div>
+                <div className="cta-box">
+                    <img src={schedule} alt="Meet the Team" />
+                    <h5>Schedule an Appointment</h5>
+                </div>
+            </div>
         </div>
     )
 };
