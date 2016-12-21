6a2c5b61a747cda61ce42d5b4ea446eaaebcb447
Ben Denzer <denzer.ben@gmail.com>
2016-12-21 01:15:17 -0800

    styled userpage

--- src/components/admin/UserPage.js
+++ src/components/admin/UserPage.js
@@ -4,21 +4,23 @@ import {parseDate} from '../../logic/shared';
 const UserPage = (props) => {
     const {users, comments, videos, params, removeComment} = props;
     const thisUser = users.length && users.filter(a => a.user_id.toString() === params.id.toString())[0];
-    let rows = <tr><td colSpan="3">No Comments Found</td></tr>;
+    let rows = <tr><td colSpan="4">No Comments Found</td></tr>;
 
     if (comments && comments.length) {
         const temp = comments.filter(a => a.user_fk.toString() === params.id.toString());
-        rows = temp.map(comment => {
-            const videoName = videos.filter(vid => vid.video_id === comment.video_fk)[0].video_title;
-            return (
-                <tr key={comment.comment_id}>
-                    <td>{parseDate(comment.comment_date)}</td>
-                    <td>{videoName}</td>
-                    <td>{comment.comment_text}</td>
-                    <td className="remove-comment" id={`comment${comment.comment_id}`}onClick={removeComment} />
-                </tr>
-            );
-        });
+        if (temp.length) {
+            rows = temp.map(comment => {
+                const videoName = videos.filter(vid => vid.video_id === comment.video_fk)[0].video_title;
+                return (
+                    <tr key={comment.comment_id}>
+                        <td>{parseDate(comment.comment_date)}</td>
+                        <td>{videoName}</td>
+                        <td>{comment.comment_text}</td>
+                        <td className="remove-comment" id={`comment${comment.comment_id}`}onClick={removeComment} />
+                    </tr>
+                );
+            });
+        }
     }
 
     return (
@@ -26,36 +28,36 @@ const UserPage = (props) => {
             <h1>{thisUser.username}</h1>
             <div id="user_dataBox">
                 {thisUser.admin ? <h4>Administrator Account</h4> : null}
-                <div><strong>Signup Date:</strong> {parseDate(thisUser.signup_date)}</div>
+                <p><strong>Signup Date:</strong> {parseDate(thisUser.signup_date)}</p>
                 {
                     thisUser.premium ?
                         <div>
-                            <div>
+                            <p>
                                 <strong>Premium Member Since: </strong>
                                 {thisUser.premium_signup_date ? parseDate(thisUser.premium_signup_date) : 'N/A'}
-                            </div>
-                            <div>
+                            </p>
+                            <p>
                                 <strong>Premium Expiration: </strong>
                                 {thisUser.premium_expiration_date ? parseDate(thisUser.premium_expiration_date) : 'N/A'}
-                            </div>
+                            </p>
                         </div>
                     : null
                 }
-                <div><strong>Comments</strong></div>
-                <table id="comments_table" className="admin-table">
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th>Video</th>
-                            <th>Comment</th>
-                            <th>Remove</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {rows}
-                    </tbody>
-                </table>
+                <p><strong>Comments</strong></p>
             </div>
+            <table id="comments_table" className="admin-table">
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Video</th>
+                        <th>Comment</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
         </div>
     );
 };
