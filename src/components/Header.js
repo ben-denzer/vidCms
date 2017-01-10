19ab801af503f892e6b71378a7dddc8e4b213d9f
Ben Denzer <denzer.ben@gmail.com>
2017-01-10 00:05:31 -0800

    moved to react-bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,68 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import logo from '../img/logo.png';
 
 const Header = (props) => {
     return (
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
-                    <a className="navbar-brand" onClick={() => browserHistory.push('/')}>
-                        <img src={logo} alt="Equinimity Energy Healing - Las Vegas" />
-                    </a>
-                </div>
-
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><a onClick={() => browserHistory.push('/')}>Home</a></li>
-                        <li><a onClick={() => browserHistory.push('/about')}>About Us</a></li>
-                        <li><a onClick={() => browserHistory.push('/videos')}>Videos</a></li>
-                        <li><a onClick={() => browserHistory.push('/contact')}>Contact</a></li>
-                        {!props.name ?
-                            <li><a id="nav_login" onClick={() => browserHistory.push("/auth/login")}>Log In</a></li> :
-                            <li><a id="nav_myaccount" onClick={() => browserHistory.push("/account")}>My Account</a></li>
-                        }
-                        {!props.name ?
-                            <li><a id="nav_signup" onClick={() => browserHistory.push("/auth/signup")}>Sign Up</a></li> :
-                            <li><a id="nav_logout" onClick={props.logout}>Log Out</a></li>
-                        }
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#">React-Bootstrap</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav pullRight>
+                    <NavItem eventKey={1} href="#">Link Right</NavItem>
+                    <NavItem eventKey={2} href="#">Link Right</NavItem>
+                    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                        <MenuItem eventKey={3.1}>Action</MenuItem>
+                        <MenuItem eventKey={3.2}>Another action</MenuItem>
+                        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
+
+
+    // return (
+    //     <nav className="navbar navbar-default">
+    //         <div className="container-fluid">
+    //             <div className="navbar-header">
+    //                 <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+    //                     <span className="sr-only">Toggle navigation</span>
+    //                     <span className="icon-bar"></span>
+    //                     <span className="icon-bar"></span>
+    //                     <span className="icon-bar"></span>
+    //                 </button>
+    //                 <a className="navbar-brand" onClick={() => browserHistory.push('/')}>
+    //                     <img src={logo} alt="Equinimity Energy Healing - Las Vegas" />
+    //                 </a>
+    //             </div>
+
+    //             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    //                 <ul className="nav navbar-nav navbar-right">
+    //                     <li><a onClick={() => browserHistory.push('/')}>Home</a></li>
+    //                     <li><a onClick={() => browserHistory.push('/about')}>About Us</a></li>
+    //                     <li><a onClick={() => browserHistory.push('/videos')}>Videos</a></li>
+    //                     <li><a onClick={() => browserHistory.push('/contact')}>Contact</a></li>
+    //                     {!props.name ?
+    //                         <li><a id="nav_login" onClick={() => browserHistory.push("/auth/login")}>Log In</a></li> :
+    //                         <li><a id="nav_myaccount" onClick={() => browserHistory.push("/account")}>My Account</a></li>
+    //                     }
+    //                     {!props.name ?
+    //                         <li><a id="nav_signup" onClick={() => browserHistory.push("/auth/signup")}>Sign Up</a></li> :
+    //                         <li><a id="nav_logout" onClick={props.logout}>Log Out</a></li>
+    //                     }
+    //                 </ul>
+    //             </div>
+    //         </div>
+    //     </nav>
+    // );
 }
 
 export default Header;
