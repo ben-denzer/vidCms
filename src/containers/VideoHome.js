defb360fef32fd4303beb86cadf4111252ee56e4
Ben Denzer <denzer.ben@gmail.com>
2017-01-14 00:38:02 -0800

    changed videoHome to stateless, generalized styling to work w/ blog posts too

--- src/containers/VideoHome.js
+++ src/containers/VideoHome.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {getAllVideos} from '../actions/contentActions'
 import VideoPlaceholder from '../components/content/VideoPlaceholder';
 
-class VideoPage extends React.Component {
-    componentDidMount() {
-        !this.props.videos.length && this.props.getAllVideos();
+const VideoHome = (props) => {
+    let videos = [];
+
+    if (props.videos.length) {
+        videos = props.videos.map(a => {
+            return <VideoPlaceholder
+                key={a.video_title}
+                id={a.video_id}
+                title={a.video_title || ''}
+                headline={a.video_headline || ''}
+                text={a.video_text || ''}
+                premium_video={a.premium}
+                video_date={a.video_date}
+                placeholder_url={a.placeholder_url}
+            />
+        });
     }
-    render() {
-        let videos = [];
-        if (this.props.videos.length) {
-            videos = this.props.videos.map(a => {
-                return <VideoPlaceholder
-                    key={a.video_title}
-                    id={a.video_id}
-                    title={a.video_title || ''}
-                    headline={a.video_headline || ''}
-                    text={a.video_text || ''}
-                    premium_video={a.premium}
-                    video_date={a.video_date}
-                    placeholder_url={a.placeholder_url}
-                />
-            });
-        }
-        return (
-            <div id="video_page_container">
-                <h1>Our Videos</h1>
-                {videos}
-            </div>
-        );
-    }
-}
+
+    return (
+        <div id="video_page_container">
+            <h1>Our Videos</h1>
+            {videos}
+        </div>
+    );
+};
 
 const mapStateToProps = (state) => {
     return {
@@ -38,10 +34,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getAllVideos: () => dispatch(getAllVideos(dispatch))
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(VideoPage);
+export default connect(mapStateToProps)(VideoHome);
